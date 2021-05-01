8bdcb41b5cb973c54939f2e74d1f26ba12bfd162
juan ariza <juanpariza195@gmail.com>
2021-05-01 10:04:07 -0500

    comentarios, sentencias, variables, constantes y uso de let

---
+++ script.js
@@ -0,0 +1,65 @@
+//hola mundo
+alert('hola mundo');
+
+//comentarios
+ 
+
+//comentario de linea
+/*comentario de mas de una linea*/
+
+//sentencias
+
+alert('hola');alert('mundo');
+
+//alert('hola')
+//alert('mundo')
+
+// variables
+
+let mensaje;
+mensaje= "hola mundo"
+//alert(mensaje)
+let a = 5;
+let b = 6;
+let suma = a + b;
+console.log("El resultado de la suma es: ",suma);
+
+b = 7
+console.log(b)
+
+let nombre = "juan";
+let nombre2 = nombre;
+
+nombre2 = "pablo";
+console.log("el nombre uno es:",nombre); 
+console.log("el nombre dos es:",nombre2);
+
+//nombrar variables con camel case
+nombreUno = "Juan";
+nombreDos = "Pablo";
+
+//nombrar como underscore
+nombre_uno = "Juan";
+nombre_dos = "Pablo";
+
+//nombres no comunes
+let $ = 1;
+let _ =2;
+
+//no puede nombrar variables
+
+/*let 1a = 3;
+let primer-nombre = 4;*/
+
+"use strict"
+
+mensaje = "hola mundo";
+
+// constantes
+
+const pi = 3.141624;
+const COLOR_ROJO = "#FFFFF";
+pi_dos = pi;
+pi_dos = 7;
+console.log(pi);
+console.log(pi_dos);
