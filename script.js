dd509f838a6c4fe12d98e1fa490141d5ac44d453
juan ariza <juanpariza195@gmail.com>
2021-05-01 15:59:58 -0500

    nullish o funcion de nulos

--- script.js
+++ script.js
@@ -216,6 +216,7 @@ string 'hola' -> NaN
 //console.log(Boolean("hola"))
 
 /*
+TABLA CONVERSIONES BOOLEANOS
 undefined -> false
 null -> false
 string '' -> false
@@ -281,3 +282,227 @@ console.log(-a);
 a = 1;
 console.log(-true);
 console.log(-false);
+
+// Asignaciones
+
+a = 2 + 3
+a = b = c = 2 + 3
+
+c = 2 + 3
+b = c
+a = b
+
+c = 3 - (a = b + 1)
+
+// Modificadores
+
+a = a + 2;
+a += 2;
+a = a - 2
+a -= 2;
+
+// incremento y decremento
+
+// ++ Incremento
+// -- Decremento
+
+a = 2;
+a ++;
+console.log(a); // 3
+
+a = 2;
+a --;
+console.log(a); //1
+
+// posicion fel incremento o decremento
+
+a = 1;
+b = ++ a;
+console.log(b);
+
+a = 1;
+b = a ++;
+c = a;
+console.log(b); //1
+console.log(c);// 2
+
+// comparaciones 
+
+console.log(2 > 1); // true
+console.log(2 == 1); // false
+//console.log(2 =!1); // true
+
+let resultado = 2 > 1;
+
+// Comparaciones entre cadenas
+
+console.log('z' > 'A');
+console.log('Show' > 'show');// false
+console.log('casa' > 'casas');// false
+console.log('casa' > 'casas'); //false
+console.log('a' > 'A');
+console.log('carro' > 'cerro');
+
+//1. La letra minuscula es mayor que la mayuscula
+//2. La palabra con mas letras despues de pasar la primera condicion es mayor que la de menos letras
+
+//comparacion de diferentes tipos
+
+// al comparar valores de diferentes tipos, JS convierte los valores a numeros
+
+//toda comparacion con NaN es falsa
+
+
+a = 0;
+b = '0';
+
+boleano_a = Boolean(a); //false
+boleano_b = Boolean(b); // true
+
+console.log(a == b);//true
+
+console.log('**************')
+console.log('2' > 1); // true
+console.log('01' == 1);//true
+console.log(true > 0); // true
+console.log(false == 0); // true
+
+//NaN
+console.log('hola' > 0)// false
+console.log('hola' < 0)// false
+console.log('hola' == 0)// false
+
+console.log('***** undefined*****')
+//UNDEFINED
+
+console.log(undefined > 0)//false
+console.log(undefined < 0)//false
+
+
+console.log('***** null*****')
+//undefined
+console.log(null > 0)//false
+console.log(null < 0)//false
+console.log(null >= 0)//false
+
+console.log('******null y undefined ==******')
+// el undefined y el null en las comparaciones de igualacion no estrictas
+// actuan diferente: son iguales entre si, pero no a ningun otro valor
+
+console.log(null == undefined); //true
+console.log(null ==5) //false
+console.log(undefined ==5)//false
+
+console.log('****** no diferencia de 0 de falso == ********')
+
+console.log(0 == false);// false
+console.log('' == false);// true
+
+// igualacion estricta
+
+console.log(0 == false); //false
+console.log( null == undefined);// false
+
+
+// sentencia IF
+
+// if de linea 
+if(4 > 5) console.log('mensaje');
+
+if(4 > 5){
+    b = 5 + 3;
+    console.log('mensaje');
+}
+
+/*
+TABLA CONVERSIONES BOOLEANOS
+undefined -> false
+null -> false
+string '' -> false
+NaN -> false
+0 -> false
+'0' -> true
+*/
+
+if('45A' /3){
+    console.log('no entro');
+}
+
+if('0'){
+    console.log('entro');
+}
+
+if(4/2){
+    console.log('entro')
+}
+
+if(1){
+    console.log('entro o no entro?');
+}
+
+let edadx
+
+if(edadx){
+    console.log('variable definida');
+}else{
+    console.log('variable indefinida');
+}
+// haga un algoritmo que dado una edad imprima true si es 
+// false si no lo es.
+
+let edad_usuario = 17;
+if(edad_usuario >= 18){
+    console.log(true);
+}else{
+    console.log(false);
+}
+// lo mismo que el anterior pero mas optimizado
+//console.log(edad_usurio>= 18)
+
+// operador ternario
+
+//condicion ? operacion verdadera : operacion falsa;
+resultado = edad_usuario >=18 ? x = 5 : x=7;
+
+// operador logico
+
+// OR
+/*
+v or v = v
+v or f = v
+f or v = v
+f or f = f
+*/
+
+console.log('*******OPERADOR OR*******');
+console.log(true || true);//true
+console.log(true || false);//true
+console.log(false || true);//true
+console.log(false || false);//false
+
+if (4 ==4 || 2 > 5){
+    console.log('verdadero');
+}
+
+// resultado = vañor_uno || valor_dos || valor_tres;
+// 1. evalua los operandos de izquierda a derecha.
+// 2. por cada operando, convierte el valor a booleano .si el resultado es true.
+// se detiene y retorna el valor original de ese operando.
+// 3. si todos los operandos han sido evaluados (todos eran falsos) retorna el ultimo operando.
+
+let comparacion_booleana = true || false || true; //true
+comparacion_booleana = '0' || '0';// '0'
+comparacion_booleana = undefined || null || NaN || '';//''
+comparacion_booleana = undefined || null || 3|| '';//3
+comparacion_booleana = undefined || 'hola' || 3|| '';//'hola'
+// resumen: devuelve el primer valor verdadero o el ultimo si todos son falsos
+
+// prevalencia u orden entre or y and
+// el AND tiene mas prevalencia que el OR
+// a && b || c && d = (a && b) || (c && d)
+
+// negacion
+
+console.log(!true);// false
+console.log(!false);//true
+console.log(!!undefined);// false == boolean(underfined)
